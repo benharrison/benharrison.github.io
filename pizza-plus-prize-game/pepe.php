a7e6186d6ff756e3da33f67b9e9d04cc11ec1cfc
benharrison <bharrison912@gmail.com>
2015-04-01 16:05:59 -0400

    removing php code

--- pizza-plus-prize-game/pepe.php
+++ pizza-plus-prize-game/pepe.php
@@ -1,19 +1,8 @@
-<?php
-ob_start();
-
-	echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-	echo "<item>\n\t";
-			echo "<play>TRUE</play>\n\t";
-			echo "<id>1</id>\n\t";
-		echo "<desc>Free Prize</desc>\n\t";
-		echo "<gameid>1234</gameid>\n\t";
-			echo "<email>Message has been sent</email>\n\t";
-//				echo "<lastplay>";
-//				echo date(TIME_FORMAT, $lastplay + (TIME_OFFSET * 3600));
-//				echo "</lastplay>\n";
-	echo "</item>\n\t";
-
-header("Content-Length: ". ob_get_length());
-// flush the output
-ob_end_flush();
-?>
+<?xml version="1.0" encoding="UTF-8"?>
+<item>
+    <play>TRUE</play>
+    <id>1</id>
+    <desc>Free Prize</desc>
+    <gameid>1234</gameid>
+    <email>Message has been sent</email>
+</item>
