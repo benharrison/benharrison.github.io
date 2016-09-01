131a56ad1e2c6231909059197894530a22f1b70a
Ben Harrison <bharrison912@gmail.com>
2016-09-01 07:11:21 -0400

    cleanup

--- demo/dribbble-streetball/README.md
+++
@@ -1,6 +0,0 @@
-Streetball
-==========
-
-Uses the dribbble api to compare the stats of two user accounts.
-
-Demo: http://benharrison.cc/dribbble-streetball
--- demo/pizza-plus-prize-game/pepe.php
+++
@@ -1,8 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<item>
-    <play>TRUE</play>
-    <id>1</id>
-    <desc>Free Prize</desc>
-    <gameid>1234</gameid>
-    <email>Message has been sent</email>
-</item>
