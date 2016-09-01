3c17040512090f7a3a02093c675598251ef92a30
Ben Harrison <bharrison912@gmail.com>
2016-09-01 07:11:39 -0400

    cleanup

---
+++ projects/dribbble-streetball/README.md
@@ -0,0 +1,6 @@
+Streetball
+==========
+
+Uses the dribbble api to compare the stats of two user accounts.
+
+Demo: http://benharrison.cc/dribbble-streetball
---
+++ projects/pizza-plus-prize-game/pepe.php
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<item>
+    <play>TRUE</play>
+    <id>1</id>
+    <desc>Free Prize</desc>
+    <gameid>1234</gameid>
+    <email>Message has been sent</email>
+</item>
