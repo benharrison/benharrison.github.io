1d492fe83bf1f47877f6e970c79683f9dc7429b8
Ben Harrison <bharrison912@gmail.com>
2016-09-07 12:55:32 -0400

    adding css lint

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require('gulp'),
-  	connect = require('gulp-connect');
+    connect = require('gulp-connect'),
+    csslint = require('gulp-csslint');
 
 gulp.task('webserver', function() {
-  	connect.server();
+    connect.server();
+});
+
+gulp.task('css', function() {
+    gulp.src('assets/css/*.css')
+        .pipe(csslint())
+        .pipe(csslint.formatter());
 });
 
 gulp.task('default', ['webserver']);
